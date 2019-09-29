68f9aaae63f2d16ef79f692ba8d2181d81521712
Toshihiro Tomita <toshihiro.tomita@paypay-corp.co.jp>
2019-09-30 05:17:04 +0900

    added requested builder

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import {GitHub} from '@actions/github';
 
 async function run() {
   try {
@@ -14,6 +15,7 @@ async function run() {
       const message = builder.build();
       console.log(message);
     }
+    
     // console.log(buildMessage(github, job, steps))
     // const webhook_url = core.getInput('webhook_url');
     // const webhook = new IncomingWebhook(webhook_url);
@@ -26,7 +28,14 @@ async function run() {
 function factory(github, job, steps) {
   switch(github.event_name) {
     case 'pull_request':
-      return new PullRequestMessageBuilder(github, job, steps);
+      switch(github.event.action) {
+        case 'rerequested':
+          return new PullRequestRequestedMessageBuilder(github, job, steps);
+        case 'opened':
+        case 'synchronize':
+        case 'reopened':
+          return new PullRequestMessageBuilder(github, job, steps);
+      }
     case 'push':
       return new PushMessageBuilder(github, job, steps);
   }
@@ -42,38 +51,38 @@ class MessageBuilder {
     this.steps = steps;
   }
   
-  build(): any {
+  async build() {
     return {
       attachments: [
         {
-          pretext: this.pretext(),
-          color: this.color(),
-          author_name: this.authorName(),
-          title: this.title(),
-          title_link: this.titleLink(),
-          text: this.text(),
-          fields: this.fields(),
-          footer: this.footer(),
-          footer_icon: this.footerIcon(),
+          pretext: await this.pretext(),
+          color: await this.color(),
+          author_name: await this.authorName(),
+          title: await this.title(),
+          title_link: await this.titleLink(),
+          text: await this.text(),
+          fields: await this.fields(),
+          footer: await this.footer(),
+          footer_icon: await this.footerIcon(),
           ts: Date.now()
         }
       ]
     }
   }
 
-  authorName(): String {
+  async authorName(): Promise<String> {
     return "";
   }
 
-  pretext(): String {
+  async pretext(): Promise<String> {
     return core.getInput("message");
   }
 
-  text(): String {
+  async text(): Promise<String> {
     return "";
   }
 
-  color(): String {
+  async color(): Promise<String> {
     switch(this.job.status) {
       case 'Failure':
         return 'danger';
@@ -84,15 +93,15 @@ class MessageBuilder {
     }
   }
 
-  titleLink(): String {
+  async titleLink(): Promise<String> {
     return `https://github.com/${this.github.repository}/commit/${this.github.sha}/checks`;
   }
 
-  title(): String {
+  async title(): Promise<String> {
     return this.github.workflow;
   }
 
-  fields() {
+  async fields() {
     return [
       {
         title: 'Repository',
@@ -107,11 +116,11 @@ class MessageBuilder {
     ]
   }
 
-  footer(): String {
+  async footer(): Promise<String> {
     return core.getInput('footer');
   }
 
-  footerIcon(): String {
+  async footerIcon(): Promise<String> {
     return core.getInput('footer_icon');
   }
 }
@@ -127,23 +136,65 @@ class PullRequestMessageBuilder extends MessageBuilder {
     super(github, job, steps);
   }
 
-  authorName(): String {
+  async authorName(): Promise<String> {
     return this.github.repository;
   }
 
-  title(): String {
+  async title(): Promise<String> {
     return this.github.event.pull_request.title;
   }
 
-  titleLink(): String {
+  async titleLink(): Promise<String> {
     return `${this.github.event.pull_request.html_url}/checks`;
   }
 
-  text(): String {
+  async text(): Promise<String> {
     return this.github.event.pull_request.body;
   }
   
-  fields(): any {
+  async fields() {
+    return []
+  }
+}
+
+
+class PullRequestRequestedMessageBuilder extends MessageBuilder {
+  gh_client: GitHub;
+  pull_request: any;
+  constructor(github, job, steps) {
+    super(github, job, steps);
+    this.gh_client = new GitHub(core.getInput('github_token'));
+  }
+
+  async build() {
+    const owner = this.github.event.repository.owner.login;
+    const pull_number = this.github.event.pull_requests[0].number;
+    const repo = this.github.event.repository.name;
+    this.pull_request = await this.gh_client.pulls.get({
+      owner,
+      pull_number,
+      repo 
+    });
+    return super.build()
+  }
+
+  async authorName(): Promise<String> {
+    return this.github.repository;
+  }
+
+  async title(): Promise<String> {
+    return this.pull_request.title;
+  }
+
+  async titleLink(): Promise<String> {
+    return `${this.pull_request.html_url}/checks`;
+  }
+
+  async text(): Promise<String> {
+    return this.pull_request.body;
+  }
+  
+  async fields() {
     return []
   }
 }
