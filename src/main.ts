7a473bc5e6e907cadd1d92a5b9fba44253a28967
Toshihiro Tomita <toshihiro.tomita@paypay-corp.co.jp>
2019-09-29 23:48:06 +0900

    updated

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,60 @@ import {wait} from './wait'
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    const github = JSON.parse(core.getInput('github'));
+    const job = JSON.parse(core.getInput('job'));
+    const steps = JSON.parse(core.getInput('steps'));
+    console.log(github);
+    console.log(job);
+    console.log(steps);
+    // const webhook_url = core.getInput('webhook_url');
+    // const webhook = new IncomingWebhook(webhook_url);
+    // await webhook.send(buildMessage(github, job))
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+// function buildMessage(github: any, job: any): any {
+//   return {
+//     attachments: [
+//       {
+//         pretext: toPretext(github, job),
+//         color: toColor(job.status),
+//         author_name: authorName(github, job),
+//         title: '',
+//         title_link: '',
+//         text: '',
+//         fields: [
+//           {
+//             title: 'Priority',
+//             value: 'High',
+//             short: false
+//           }
+//         ],
+//         image_url: '',
+//         footer: 'Github Actions',
+//       }
+//     ]
+//   }
+// }
+
+// function authorName(github: any, job: any): String {
+//   return github.workflow;
+// }
+
+// function toPretext(github: any, job: any): String {
+//   return "";
+// }
+
+// function toColor(job_status: String): String {
+//   switch(job_status) {
+//     case 'Failure':
+//       return 'danger';
+//     case 'Success':
+//       return 'good';
+//     default:
+//       return '#555555';
+//   }
+// }
 run();
